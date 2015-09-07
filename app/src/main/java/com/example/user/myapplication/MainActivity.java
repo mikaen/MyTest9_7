b4616b7a326917b4674bc2e144e032af724a4eac
mikaen <mikaen2891@naver.com>
2015-09-07 10:51:36 +0900

    int r plus int c also plus

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 int r;
+    int c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
